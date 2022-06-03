1e2c80c24956ae2f0826857a7e81158cd2a8dc87
Roshan Veerabhadraiah <veerabha@synopsys.com>
2022-06-03 19:06:18 +0530

    versioning & validation

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -21,6 +21,9 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min=4, max=100)
     private String title;
     @Column(length=1000000)
     @Lob
